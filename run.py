ef6734012a6eb30786331912800ed0f0faeb7aaf
5undel <max.sundel@Choice.se>
2021-10-12 19:21:02 +0000

    Create the playboard for the game

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def render(board_width, board_height):
+    print("+" + "-" * board_width + "+")
+
+    for i in range(board_height):
+        print("|" + " " * board_width + "|")
+
+    print("+" + "-" * board_width + "+")
+
+if __name__ == "__main__":
+    render(8, 8)
+
+    inp = input("Wher do you want to shoot?\n")
+    xstr, ystr = inp.split(",")
+    x = int(xstr)
+    y = int(ystr)
+   
