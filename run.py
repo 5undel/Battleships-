892252e0036cdc2f56c53df29ac1db4ff65f82a6
5undel <max.sundel@Choice.se>
2021-10-12 19:58:54 +0000

    Make boardplay bigger to see

--- run.py
+++ run.py
@@ -1,14 +1,23 @@
 
-def render(board_width, board_height):
-    print("+" + "-" * board_width + "+")
+def render(board_width, board_height, shots):
+    print("+" + " _ " * board_width + "+")
 
-    for i in range(board_height):
-        print("|" + " " * board_width + "|")
+    for y in range(board_height):
+        row = []
+        for x in range(board_width):
+            if (x,y) in shots:
+                ch = "x"
+            else:
+                ch = " "
+            row.append(ch)
+        print("| " + " " + " " + " " + " " + " " + " " + " " + " ".join(row) + " |")
+    
 
-    print("+" + "-" * board_width + "+")
+    print("+" + " _ " * board_width + "+")
+
 
 if __name__ == "__main__":
-    render(8, 8)
+    render(8, 8, [(3,1), (4,3), (3,5)])
 
     inp = input("Wher do you want to shoot?\n")
     xstr, ystr = inp.split(",")
