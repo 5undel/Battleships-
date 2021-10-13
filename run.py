1744fdac0a816668716fa05ee967f79564be0ca5
5undel <max.sundel@Choice.se>
2021-10-13 19:31:14 +0000

    Render the gameborde and user input

--- run.py
+++ run.py
@@ -1,25 +1,33 @@
+class Battleship(object):
 
+    def __init__(self)
+
+# Game board layout
 def render(board_width, board_height, shots):
-    print("+" + " _ " * board_width + "+")
+    header = "+" + "-" * board_width + "+"
+    print(header)
 
+    shots_set = set(shots)
     for y in range(board_height):
         row = []
         for x in range(board_width):
-            if (x,y) in shots:
+            if (x,y) in shots_set:
                 ch = "x"
             else:
                 ch = " "
             row.append(ch)
-        print("| " + " " + " " + " " + " " + " " + " " + " " + " ".join(row) + " |")
+        print("| " + "0".join(row) + " |")    
+
+    print(header)
     
-
-    print("+" + " _ " * board_width + "+")
-
-
 if __name__ == "__main__":
-    render(8, 8, [(3,1), (4,3), (3,5)])
+    shots = []
 
-    inp = input("Wher do you want to shoot?\n")
-    xstr, ystr = inp.split(",")
-    x = int(xstr)
-    y = int(ystr)
+    while True:
+        inp = input("Take your shot?\n")
+        xstr, ystr = inp.split(",")
+        x = int(xstr)
+        y = int(ystr)
+
+        shots.append((x,y))
+        render(8, 8, shots)
