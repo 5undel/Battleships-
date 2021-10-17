621d7ecc015a212e6b1d2914550e8bc0592cae09
5undel <max.sundel@Choice.se>
2021-10-17 13:37:12 +0000

    Create hit,mis,comp funktion and add 💥,💀 to showe on display

--- run.py
+++ run.py
@@ -1,14 +1,21 @@
 print("      BATTLESHIP")
 print("     0  1  2  3  4  5  6  7  8  9")
 
-hit = [21]
+hit = [21,22,]
+miss = [20,24,12,13]
+comp = [23]
+
 place = 0
 for x in range(10):
     row = ""
     for y in range(10):
         ch = " _ "
-        if place == 21:
+        if place in hit:
+            ch ="💥 "
+        elif place in miss:
             ch =" x "
+        elif place in comp:
+            ch ="💀 "
         row = row + ch
         place = place + 1
     print(x, " ", row)
