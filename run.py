121eb48982fb6b98718e51f8c5a9f713fc99b59d
5undel <max.sundel@Choice.se>
2021-10-17 14:18:59 +0000

    Add boat1 to game

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
+def get_shot(guesses):
+
+    print("Add a number between 0 and 99")
+    ok = "n"
+    while ok == "n":
+        try:
+            shot = input("Take your shot:\n")
+            shot = int(shot)
+            if shot < 0 or shot > 99:
+                print("incorrect number, please try again!")
+            elif shot in guesses:
+                print("incorrect number, used before!")
+            else:
+                ok = "y"
+                break
+        except:
+            print("incorrect entry - please enter again!")
+    return shot
+    
+
 def show_board(hit,miss,comp):
     print("      BATTLESHIP")
     print("     0  1  2  3  4  5  6  7  8  9")
@@ -17,10 +37,37 @@ def show_board(hit,miss,comp):
             row = row + ch
             place = place + 1
         print(x, " ", row)
+
+
+def check_shot(shot,boat1,hit,miss,comp):
+
+    if shot in boat1:
+        boat1.remove(shot)
+        if len(boat1) > 0:
+            hit.append(shot)
+        else:
+            comp.append(shot)
+
+    else:
+        miss.append(shot)
+
+    return boat1,hit,miss,comp
+
     
 
-hit = [21,22,]
-miss = [20,24,12,13]
-comp = [23]
+boat1 = [45,46,47]
+boat2 = [6,16,26]
+hit = []
+miss = []
+comp = []
 
-show_board(hit,miss,comp)
+for i in range(10):
+    guesses = hit + miss + comp
+    shot = get_shot(guesses)
+    boat1,boat2,hit,miss,comp = check_shot(shot,boat1,boat2,hit,miss,comp)
+    show_board(hit,miss,comp)
+
+    if len(boat1) < 1 and boat2:
+        print("You've won the game!!!")
+        break
+print("finished")
